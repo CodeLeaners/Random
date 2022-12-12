d2086143c3e6b574f8d0bd1a98564793ac13cc0c
Ishita Singhal <ishitasinghal08@gmail.com>
2022-12-12 17:21:32 +0530

    added solution 1

--- HollowDiamond.py
+++ HollowDiamond.py
@@ -1,7 +1,25 @@
-   *
-  * *
- *   *
-*     *
- *   *
-  * *
-   *
+#    *
+#   * *
+#  *   *
+# *     *
+#  *   *
+#   * *
+#    *
+
+# n is any random number 
+
+n = 7
+space = n//2
+midspace = 1
+result = []
+
+result.append(" "*space + "*")
+
+space -= 1
+for i in range(n//2):
+    result.append(" "*space + "*" + " "*midspace + "*")
+    space -= 1
+    midspace += 2
+
+print(*result, sep="\n")
+print(*result[:-1:][::-1], sep="\n")
